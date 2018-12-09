7c3f72807975800cf2daa3bbd462d48b6cf0cea9
Adam Kariv <adam.kariv@gmail.com>
2018-12-09 10:39:53 +0200

    Fix datapackage source url

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)
 blueprint = apies_blueprint(app,
     [
-        'http://pipelines/data/publications_for_es/datapackage.json'
+        'http://pipelines/data/published_in_es/datapackage.json'
     ],
     elasticsearch.Elasticsearch([dict(host=ES_HOST, port=ES_PORT)], timeout=60),
     INDEX_NAME,
