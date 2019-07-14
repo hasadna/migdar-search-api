b6e5cf25c0cec4af5d8c7bdb62de0f3f00d1da40
Adam Kariv <adam.kariv@gmail.com>
2019-07-14 17:07:26 +0300

    Bump apies version

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ from flask_cors import CORS
 
 from apies import apies_blueprint
 
+BASE = 'http://pipelines/data/{}_in_es/datapackage.json'
+# BASE = 'http://api.yodaat.org/data/{}_in_es/datapackage.json'
 ES_HOST = os.environ.get('ES_HOST', 'localhost')
 ES_PORT = int(os.environ.get('ES_PORT', '9200'))
 INDEX_NAME = os.environ.get('INDEX_NAME', 'migdar')
@@ -15,9 +17,9 @@ app = Flask(__name__)
 CORS(app)
 blueprint = apies_blueprint(app,
     [
-        'http://pipelines/data/publications_in_es/datapackage.json',
-        'http://pipelines/data/orgs_in_es/datapackage.json',
-        'http://pipelines/data/datasets_in_es/datapackage.json',
+        BASE.format('publications'),
+        BASE.format('orgs'),
+        BASE.format('datasets'),
     ],
     elasticsearch.Elasticsearch([dict(host=ES_HOST, port=ES_PORT)], timeout=60),
     INDEX_NAME,
